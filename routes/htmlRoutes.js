16543c4c99f96ce578183acc3372250ed3980c3b
KJ <kj3641402@gmail.com>
2021-04-09 22:34:39 -0400

    changed index location in api route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,6 +13,6 @@ module.exports = (app) => {
   });
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
   });
 };
