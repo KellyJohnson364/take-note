a377b4aa0b7f029893f1ccb044300365f7276b93
KJ <kj3641402@gmail.com>
2021-04-09 22:28:12 -0400

    moved folders around

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+// DEPENDENCIES
+
+
+const path = require('path');
+
+// ROUTING
+
+module.exports = (app) => {
+  // => HTML GET Requests
+  
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+};
