dc42429eba12e5a3127af86b2e80cdbc50b4046b
KJ <kj3641402@gmail.com>
2021-04-09 22:43:25 -0400

    I messed it up again

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,7 +9,7 @@ module.exports = (app) => {
   // => HTML GET Requests
   
   app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
 
   app.get('*', (req, res) => {
