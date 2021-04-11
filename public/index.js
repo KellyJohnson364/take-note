0cd78f8684c17fb1c734ff9782ab07f0a89bce66
KJ <kj3641402@gmail.com>
2021-04-10 21:15:19 -0400

    fooled around with uniqid

--- public/index.js
+++ public/index.js
@@ -102,7 +102,7 @@ const handleNoteDelete = (e) => {
 const handleNoteView = (e) => {
   e.preventDefault();
   activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
-  activeNote.setAttribute("id",uniqid);
+  activeNote.setAttribute("id",uniqid());
   renderActiveNote();
 };
 
