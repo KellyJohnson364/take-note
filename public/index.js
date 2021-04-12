0dd6eb4d892fdb5af161aa5e267cd6240feab751
KJ <kj3641402@gmail.com>
2021-04-11 22:16:49 -0400

    .readonly

--- public/index.js
+++ public/index.js
@@ -64,8 +64,8 @@ const renderActiveNote = () => {
     noteTitle.value = activeNote.title;
     noteText.value = activeNote.text;
   } else {
-    noteTitle.setAttribute('readonly', false);
-    noteText.setAttribute('readonly', false);
+    noteTitle.readOnly = false;
+    noteText.readOnly = false;
     noteTitle.value = '';
     noteText.value = '';
   }
